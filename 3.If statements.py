a4114ea82a605fb52cd8294fd745450497808fc8
Lixcon11 <106622009+Lixcon11@users.noreply.github.com>
2024-10-04 00:32:04 +0200

    More if logic

--- 3.If statements.py
+++ 3.If statements.py
@@ -9,3 +9,12 @@ else:
     print("a is greater than b")
 
 print("A") if a > b else print("=") if a == b else print("B")
+
+passwordValid = True
+
+if passwordValid:
+    print("Password is valid")
+    passwordValid = False
+
+if passwordValid != True:
+    print("Password is reseted")
