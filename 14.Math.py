8d97446e3c318b1b5eeea3a7f3f464cdc5f3c9ea
Lixcon11 <106622009+Lixcon11@users.noreply.github.com>
2024-11-19 19:35:45 +0100

    Math

---
+++ 14.Math.py
@@ -0,0 +1,15 @@
+import math
+
+number_series = (20, 3, 11.1, -66, 0, 235)
+
+print(max(number_series))
+
+print(min(number_series))
+
+print(pow(number_series[0], number_series[1]))
+
+print(math.sqrt(number_series[len(number_series)-1]))
+
+print(math.ceil(number_series[2]))
+
+print(math.floor(number_series[2]))
